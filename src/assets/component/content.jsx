85a069473ab661ee927f3835a2ac1757693cfc87
RafalDigital <rafaldigitall@gmail.com>
2026-04-05 14:51:58 +0700

    update

--- src/assets/component/content.jsx
+++ src/assets/component/content.jsx
@@ -35,7 +35,7 @@ function Menu() {
                     </button>
                 </div>
                 <input type="text" placeholder="Type To Search" className="bg-secondary rounded-lg w-60 h-8 px-2 text-white border-option-5 border hover:bg-option-6 focus:outline-2 focus:outline-option-5 transition-all"/>
-
+                <FilterMenu isOpen={isOpen}/>
             </div>
         </>
     )
@@ -52,6 +52,39 @@ function Filter({content}) {
             <button onClick={toggleClick} className={` rounded-lg w-fit h-8 px-5 text-white border-option-5 border hover:bg-option-6 transition-all cursor-pointer ${isClicked?'bg-secondary':'bg-primary'}`}>
                 {content}
             </button>
+        </>
+    )
+}
+
+function FilterMenu({isOpen}) {
+    return(
+        <>
+            <div className={`absolute flex justify-center flex-wrap gap-2 px-3 py-3 max-w-[730px] bg-option-6 rounded-lg border-option-5 border transition-all ease-in-out duration-300 ${isOpen? 'opacity-100 top-10 pointer-events-auto': 'opacity-0 pointer-events-none top-12'}`}>
+                <Filter content={'Hotel'}/>
+                <Filter content={'Hottest'}/>
+                <Filter content={'UMKM'}/>
+                <Filter content={'Food'}/>
+                <Filter content={'Restourant'}/>
+                <Filter content={'Testing AJAH'}/>
+            </div>
+        </>
+    )
+}
+
+function ProductsWrapper() {
+    return(
+        <>
+            <div className="grid grid-cols-4 gap-4 w-full max-w-7xl h-fit py-5">
+
+            </div>
+        </>
+    )
+}
+
+function Products() {
+    return(
+        <>
+        
         </>
     )
 }
