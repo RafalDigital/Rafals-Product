97af693ebf6e4a300ba3d3ecc7c25329899ebc74
RafalDigital <rafaldigitall@gmail.com>
2026-04-05 16:56:57 +0700

    Update Content

--- src/assets/component/content.jsx
+++ src/assets/component/content.jsx
@@ -2,14 +2,24 @@
 import { useState } from "react"
 import { RiMore2Fill } from '@remixicon/react';
 
+const stackColors = {
+  HTML: "15 100% 55%",
+  CSS: "210 100% 50%",
+  JavaScript: "50 100% 50%",
+  React: "190 90% 60%",
+  Tailwind: "190 90% 50%",
+  NodeJS: "110 50% 40%",
+};
+
 
 export default function Content() {
     return (
         <>
-            <section className="relative h-96 w-full bg-primary flex items-center justify-center">
+            <section className="relative h-fit pb-10 w-full bg-primary flex items-center justify-center">
                 <div className="relative w-full h-full max-w-7xl flex items-center flex-col">
                     <div className="absolute top-0 w-11/12 h-0.5 bg-option-5"></div>
                     <Menu/>
+                    <ProductsWrapper/>
                 </div>
             </section>
         </>
@@ -59,7 +69,7 @@ function Filter({content}) {
 function FilterMenu({isOpen}) {
     return(
         <>
-            <div className={`absolute flex justify-center flex-wrap gap-2 px-3 py-3 max-w-[730px] bg-option-6 rounded-lg border-option-5 border transition-all ease-in-out duration-300 ${isOpen? 'opacity-100 top-10 pointer-events-auto': 'opacity-0 pointer-events-none top-12'}`}>
+            <div className={`absolute flex justify-center flex-wrap gap-2 px-3 py-3 max-w-[45.6rem] bg-option-6 rounded-lg border-option-5 border transition-all ease-in-out duration-300 ${isOpen? 'opacity-100 top-10 pointer-events-auto': 'opacity-0 pointer-events-none top-12'}`}>
                 <Filter content={'Hotel'}/>
                 <Filter content={'Hottest'}/>
                 <Filter content={'UMKM'}/>
@@ -74,17 +84,55 @@ function FilterMenu({isOpen}) {
 function ProductsWrapper() {
     return(
         <>
-            <div className="grid grid-cols-4 gap-4 w-full max-w-7xl h-fit py-5">
-
+            <div className="grid grid-cols-4 gap-2 place-items-center w-full max-w-[75rem] h-fit py-5 mt-2.5">
+                <Products stacks={['JavaScript','HTML','CSS','NodeJS']}/>
             </div>
         </>
     )
 }
 
-function Products() {
+function Products({stacks = []}) {
     return(
         <>
-        
+            <div className="group bg-secondary border border-option-5 rounded-2xl overflow-hidden w-11/12 px-0.5 pt-3 max-w-xs transition-all hover:shadow-lg">
+                <div className="aspect-[4/3] overflow-hidden w-11/12 mx-auto border border-option-5 rounded-sm">
+                    <img src="" className="w-full h-full object-cover group-hover:scale-105 transition-all duration-300 border-none"/>
+                </div>
+                <div className="p-4 flex flex-col gap-2">
+                    <h1 className="text-white font-semibold text-lg">Title</h1>
+                    <p className="text-white text-sm line-clamp-2">Lorem ipsum dolor sit amet</p>
+                    <div className="w-full flex gap-1">
+                        {stacks.slice(0, 3).map((stack, index) => (
+                            <Stack stack={stack} index={index}/>
+                        ))}
+                        {stacks.length > 3 && (
+                            <span className="text-white text-sm">+{stacks.length - 3}</span>
+                        )}
+                    </div>
+
+                    <button className="mt-2 bg-primary rounded-lg py-1.5 text-white border-option-5 border hover:bg-option-6 active:scale-95 transition-all cursor-pointer">Open</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+function Stack({index, stack}) {
+     const color = stackColors[stack] || "0 0% 40%";
+
+    return(
+        <>
+            <span
+            key={index}
+            className="flex items-center text-[10px] px-2 py-0.5 rounded-md"
+            style={{
+                backgroundColor: `hsl(${color} / 0.15)`, // soft bg
+                border: `1px solid hsl(${color} / 0.4)`,
+                color: `hsl(${color})`,
+            }}
+            >
+            {stack}
+            </span>
         </>
     )
 }
