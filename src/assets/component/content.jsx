e2ba66ec4627bcd23941ecf38dc9a461be7aa369
RafalDigital <rafaldigitall@gmail.com>
2026-04-05 21:39:07 +0700

    Update Content

--- src/assets/component/content.jsx
+++ src/assets/component/content.jsx
@@ -90,16 +90,16 @@ function ProductsWrapper() {
     return(
         <>
             <div className="grid grid-cols-4 gap-2 place-items-center w-full max-w-[75rem] h-fit py-5 mt-2.5">
-                <Products stacks={['JavaScript','HTML','CSS','NodeJS']} title={'Title'} desc={'Deskripsi'} btn={'Coba La Buka'}/>
+                <Products stacks={['JavaScript','HTML','CSS','NodeJS']} title={'Title'} desc={'Deskripsi'} btn={'Coba La Buka'} id={1}/>
             </div>
         </>
     )
 }
 
-function Products({stacks = [], title, desc, btn}) {
+function Products({stacks = [], title, desc, btn, id}) {
     return(
         <>
-            <div className="group bg-secondary border border-option-5 rounded-2xl overflow-hidden w-11/12 px-0.5 pt-3 max-w-xs transition-all hover:shadow-lg">
+            <div key={id} className="group bg-secondary border border-option-5 rounded-2xl overflow-hidden w-11/12 px-0.5 pt-3 max-w-xs transition-all hover:shadow-lg">
                 <div className="aspect-[4/3] overflow-hidden w-11/12 mx-auto border border-option-5 rounded-md">
                     <img src="" className="w-full h-full object-cover group-hover:scale-105 transition-all duration-300 border-none rounded-md"/>
                 </div>
@@ -142,27 +142,72 @@ function Stack({index, stack}) {
     )
 }
 
-// FULL INFO PRODUCT COMPONENT
-function FullProduct() {
+function Section({index, section = []}) {
     return(
         <>
-            <div className="fixed w-full h-dvh bg-black/50 z-20 flex justify-center items-center">
-                <div className="h-[80dvh] w-5xl py-5 px-5 bg-secondary rounded-xl border border-option-5 flex gap-5">
+            <span
+            key={index}
+            className="flex items-center text-[10px] text-white px-2 py-0.5 rounded-md bg-option-6 border border-option-5"
+            >
+            {section}
+            </span>
+        </>
+    )
+}
+
+// FULL INFO PRODUCT COMPONENT
+function FullProduct({ stacks = ['JavaScript', 'HTML', 'CSS','React','Tailwind','NodeJS'], sections =['Navbar','Hero','About','Products','Contact','footer']}) {
+    return(
+        <>
+            <div className="hidden w-full h-dvh bg-black/50 z-20 flex justify-center items-center">
+                <div className="overflow-hidden relative h-[80dvh] max-h-[500px] w-5xl py-5 px-5 bg-secondary rounded-xl border border-option-5 flex gap-10">
+
                     <div className="w-full h-full">
-                        <div className="group w-full h-3/4 overflow-hidden border border-option-5 rounded-lg">
+                        <div className="group w-full h-10/12 overflow-hidden border border-option-5 rounded-lg">
                             <img src="" alt="" className="w-full h-full group-hover:scale-105"/>
                         </div>
-                        <div className="flex flex-col gap-2 w-full mt-4">
+                            <h1 className="text-white font-semibold text-3xl mt-5 ml-2 font-playfairdisplay">Simple Minimalist - 1</h1>
+                        
+                    </div>
+
+                    <div className="w-0.5 h-11/12 absolute bg-option-5 left-1/2 -translate-x-1/2 rounded-md"></div>
+
+                    <div className="w-full h-full flex flex-col justify-between">
+                        <div className="h-3/4 flex flex-col">
+
+                            <div className="h-fit pb-2.5 max-h-3/4">
+                                <p className="text-white text-base font-medium mb-2">Desc</p>
+                                <p className="text-white text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, sunt consequatur! Velit quod dolores suscipit nemo exercitationem reiciendis vitae in vel, maxime quos voluptate commodi! Reprehenderit possimus ipsam sapiente reiciendis. </p>
+                                <div className="h-0.5 w-full rounded-2xl bg-option-5 mt-2.5"></div>
+                            </div>
+
+                            <div className="pb-2.5">
+                                <p className="text-white text-base font-medium mb-2">Stack</p>
+                                <div className="w-full flex gap-1">
+                                    {stacks.map((stack, index) => (
+                                    <Stack stack={stack} index={index}/>
+                                    ))}
+                                </div>
+                                <div className="h-0.5 w-full rounded-2xl bg-option-5 mt-2.5"></div>
+                            </div>
+
+                            <div>
+                                <p className="text-white text-base font-medium mb-2">Section</p>
+                                <div className="w-full flex gap-1">
+                                    {sections.map((section, index) => (
+                                    <Section index={index} section={section}/>
+                                    ))}
+                                </div>
+                                <div className="h-0.5 w-full rounded-2xl bg-option-5 mt-2.5"></div>
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-2 w-full">
+                            <p className="text-xl text-option-1 font-medium"><span>$</span>5000</p>
                             <button className="mt-1 bg-primary rounded-lg py-1.5 text-white border-option-5 border hover:bg-option-6 active:scale-95 transition-all cursor-pointer">Visit Preview</button>
-                            <button className="mt-1 bg-primary rounded-lg py-1.5 text-white border-option-5 border hover:bg-option-6 active:scale-95 transition-all cursor-pointer">Buy</button>
+                            <button className="mt-1 bg-option-1 rounded-lg py-1.5 text-white border-option-1 border hover:bg-option-6 active:scale-95 transition-all cursor-pointer">Buy</button>
                         </div>
                     </div>
-                    <div className="w-full h-full">
-                        <h1 className="text-white font-semibold text-3xl">Title</h1>
-                        <p>Desc</p>
-                        <span>Stack</span>
-                        <p>Price</p>
-                    </div>
+
                 </div>
             </div>
         </>
