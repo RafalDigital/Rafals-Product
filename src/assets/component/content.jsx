5b46f084d7cc4056ed265b4f31881146cc52c07f
RafalDigital <rafaldigitall@gmail.com>
2026-04-05 14:29:21 +0700

    update

--- src/assets/component/content.jsx
+++ src/assets/component/content.jsx
@@ -1,5 +1,6 @@
 // IMPORT
-import { RiArrowDownLongLine } from "@remixicon/react"
+import { useState } from "react"
+import { RiMore2Fill } from '@remixicon/react';
 
 
 export default function Content() {
@@ -9,10 +10,6 @@ export default function Content() {
                 <div className="relative w-full h-full max-w-7xl flex items-center flex-col">
                     <div className="absolute top-0 w-11/12 h-0.5 bg-option-5"></div>
                     <Menu/>
-                    <div className="absolute flex flex-col items-center cursor-pointer h-max text-option-5">
-                        <div className="w-1 h-20 bg-option-5 rounded-xs translate-y-2.5 transition-all ease-in-out duration-300"></div>
-                        <RiArrowDownLongLine size={46} className="transition-all ease-in-out duration-300" />
-                    </div>
                 </div>
             </section>
         </>
@@ -20,11 +17,41 @@ export default function Content() {
 }
 
 function Menu() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggleOpen() {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <>
-            <div className="mt-10 w-11/12 flex justify-end items-center gap-2">
-                <input type="text" placeholder="Type To Search" className="bg-primary rounded-lg w-60 h-8 px-2 text-white border-option-5 border hover:bg-option-6 focus:outline-2 focus:outline-option-5 transition-all"/>
+            <div className="relative mt-10 w-11/12 flex justify-between items-center gap-2">
+                <div className="flex justify-between items-center gap-2">
+                    <Filter content={'Latest'}/>
+                    <Filter content={'Finance'}/>
+                    <Filter content={'Minimalist'}/>
+                    <button onClick={toggleOpen} className={`rounded-lg w-fit h-8 px-1 ml-1 text-white border-option-5 border hover:bg-option-6 transition-all cursor-pointer ${isOpen?'bg-secondary':'bg-primary'}`}>
+                        < RiMore2Fill  />
+                    </button>
+                </div>
+                <input type="text" placeholder="Type To Search" className="bg-secondary rounded-lg w-60 h-8 px-2 text-white border-option-5 border hover:bg-option-6 focus:outline-2 focus:outline-option-5 transition-all"/>
+
             </div>
         </>
     )
 } 
+
+function Filter({content}) {
+    const [isClicked, setIsClicked] = useState(false);
+
+    function toggleClick() {
+        setIsClicked(!isClicked)
+    }
+    return(
+        <>
+            <button onClick={toggleClick} className={` rounded-lg w-fit h-8 px-5 text-white border-option-5 border hover:bg-option-6 transition-all cursor-pointer ${isClicked?'bg-secondary':'bg-primary'}`}>
+                {content}
+            </button>
+        </>
+    )
+}
--- src/assets/component/hero.jsx
+++ src/assets/component/hero.jsx
@@ -5,20 +5,26 @@ export default function Hero() {
     return (
         <>
             <section className="relative h-dvh max-h-[500px] w-full bg-linear-to-b from-option-3 to-primary flex items-center justify-center">
-                <div className="relative w-11/12 h-10/12 max-w-7xl bg-secondary border-option-5 border flex items-center justify-center flex-col rounded-4xl">
-                    <h1 className="text-white text-5xl font-playfairdisplay font-bold">RAFAL'S PRODUCTS</h1>
-                    <p className="text-white">Web Templates and More</p>
+                <div className="relative w-11/12 h-10/12 max-w-7xl backdrop-blur-2xl border-option-5 border flex items-center justify-center flex-col rounded-4xl z-10 overflow-hidden">
+                    <h1 className="text-white text-5xl font-playfairdisplay font-bold select-none hover:scale-110 active:blur-xs hover:tracking-widest transition-all ease-in-out duration-300">RAFAL'S PRODUCTS</h1>
+                    <p className="text-white select-none hover:scale-110 hover:tracking-widest active:blur-xs transition-all ease-in-out duration-300">Web Templates and More</p>
                     <div className="absolute w-full bottom-2 left-8 right-8 flex gap-2.5">
                         <Link logo={RiInstagramLine} size={16}/>
                         <Link logo={RiGithubLine} size={16}/>
                     </div>
-                    <div className="group absolute flex flex-col items-center cursor-pointer h-max text-white right-10 top-20">
-                        <div className="w-1 h-20 bg-white rounded-xs translate-y-2.5 transition-all ease-in-out duration-300 group-hover:h-40"></div>
+                    <div className="group absolute flex flex-col items-center cursor-pointer h-max text-white right-10 top-10">
+                        <div className="w-1 h-20 bg-white rounded-xs translate-y-2.5 transition-all ease-in-out duration-300 group-hover:h-60"></div>
                         <RiArrowDownLongLine size={46} />
-                        <p>Scroll</p>
-                        <p>Down</p>
+                        <p className='group-hover:tracking-widest transition-all ease-in-out duration-300'>Scroll</p>
+                        <p className='group-hover:tracking-widest transition-all ease-in-out duration-300 delay-100'>Down</p>
                     </div>
                 </div>
+                <div className='absolute w-11/12 h-10/12 max-w-7xl flex items-center justify-center flex-col rounded-4xl overflow-hidden'>
+                    <div className='w-20 h-10/12 opacity-50 bg-white absolute z-[1] rotate-90'></div>
+                </div>
+                <div className='w-11/12 h-10/12 max-w-7xl bg-secondary rounded-4xl absolute z-0'></div>
+
+
             </section>
         </>
     )
